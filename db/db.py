bee3075fcbf9eab8743b48be0404fae65354b1f7
dpa <dakimchuk@bitfirenetworks.com>
2023-11-21 19:13:44 -0500

    fix pathing for db

--- db/db.py
+++ db/db.py
@@ -6,6 +6,6 @@ from playhouse.migrate import *
 
 # Connect to the database
 # Other modules in project should use 'from db.db import db' to access it
-db = SqliteDatabase(os.path.abspath(__file__ + "/../../default_db.sqlite"))
+db = SqliteDatabase(os.path.abspath(__file__ + "/../../presets/default_db.sqlite"))
 migrator = SqliteMigrator(db)
 db.connect()
